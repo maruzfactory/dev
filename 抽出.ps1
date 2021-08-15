eac115f3bef3739c48ea54684d5d472997cec2a6
maruzfactory <maruzfactory@gmail.com>
2021-08-16 08:29:31 +0900

    Update and rename 抽出 to 抽出.ps1

--- 抽出.ps1
+++ 抽出.ps1
@@ -2,3 +2,4 @@
 $csv | Where-Object {$_.駅名 -match '.*信濃.*'} | Export-Csv ./result.csv -Encoding Default -NoTypeInformation
 
 #参照　https://hebodj.net/2021/03/27/powershellによるcsvファイルからのデータ抽出/
+#参照　https://miyalog.info/technology/1357/
