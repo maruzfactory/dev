c5e011c658d9d7a5380cc932a13009cc7bbc5fcd
maruzfactory <maruzfactory@gmail.com>
2022-06-18 21:57:07 +0900

    Update 抽出.ps1

--- powershell/抽出.ps1
+++ powershell/抽出.ps1
@@ -6,3 +6,15 @@ $csv | Where-Object {$_.駅名 -match '.*信濃.*'} | Export-Csv ./result.csv -E
 
 #列抽出
 import-csv test.csv  -encoding Default | select "氏名","年齢" | export-csv out.csv -encoding Default　-NoTypeInformation
+
+#----------
+#test.csvを読込1～9列目の列数を追加。
+#1列目をout.csvで抽出
+import-csv data.csv -Encoding Default -header @(1..9) |select "1","4" | export-csv out.csv -Encoding Default -NoTypeInformation
+
+#out.csvを読込、1行目（列数を追加した行）を飛ばしてex-result.csvに抽出
+get-content out.csv -Encoding Default | select -skip 1 | foreach {$_.Replace('"','')} | out-file ex-result.csv
+
+#tsvをcsvに変換
+import-csv ex-result.csv -Encoding Default | export-csv result.csv -Encoding Default -NoTypeInformation
+
